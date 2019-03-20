28c889f6170e7b95f3020a15348c42231d7eabea
Mariah Alderson <mariah@Mariahs-MBP.fanshawec.ca>
2019-03-20 13:34:56 -0400

    test

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+http.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
