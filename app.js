0420d8841e5d61e515caa4f4d9818524d63818a0
Emma Blue <emmajaeblue@gmail.com>
2019-03-27 13:27:23 -0400

    chat app base

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')();
 
 const port = process.env.PORT || 3000;
 
+//tell express where our static files are
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-http.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`app is running on port ${port}`);
+});
+io.attach(server);
+
+// socket.io chat app stuff to follow
+
+io.on('connection', function(socket){
+
+  console.log('a user has connected', socket);
+  socket.emit('connected', {socketID: `${socket.id}`, message: 'new connection'});
+
+  //listen for an incoming message from anyone connected to the app
+
+  socket.on('chat message', function(msg){
+
+    // send the message to everyone connected to the app
+    console.log('message: ', msg, 'socket:', socket.id);
+    io.emit('chat message', {id: `${socket.id}`, message: msg});
+
+  })
+
+  socket.on('disconnect', function(){
+
+      console.log('a user has disconnected');
+
+  });
+
 });
